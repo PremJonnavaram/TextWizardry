8035b07364763fe325a976eaa556b6c54b13d27f
Prem Kumar Reddy Jonnavaram <premjonnavaram4990@gmail.com>
2024-09-09 18:23:46 +0530

    made some chnages

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         <div className="container my-3">
           <Routes>
             <Route exact path='/' element={
-              <Textform showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={Mode} />
+              <Textform showAlert={showAlert} heading="Try TextWizardry - Word Counter, Character Counter, Remove extra spaces" mode={Mode} />
             }/>
             <Route exact path='/about' element={
               <About mode={Mode} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,11 +15,7 @@ export default function Navbar(props) {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                {/* <NavLink  exact className="nav-link fw-medium" aria-current="page" to="/">Home</NavLink> */}
-                                <a  exact className="nav-link fw-medium" aria-current="page" href="/">Home</a>
-                                
-                            </li>
+    
                             <li className="nav-item">
                                 {/* <NavLink  exact className="nav-link fw-medium" aria-current="page" to="/">Home</NavLink> */}
                                 <a  exact className="nav-link fw-medium" aria-current="page" href="/about">About</a>
